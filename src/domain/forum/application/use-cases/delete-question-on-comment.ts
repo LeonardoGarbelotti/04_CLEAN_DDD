75e8281eec2a3e9ec6c3253677aa81757210b92f
Leonardo Gonçalves <leo13garbelotti@hotmail.com>
2024-01-29 11:09:21 -0300

    Fix: moved erros folder |  Add: send and read notification

--- src/domain/forum/application/use-cases/delete-question-on-comment.ts
+++ src/domain/forum/application/use-cases/delete-question-on-comment.ts
@@ -1,7 +1,7 @@
 import { Either, left, right } from '@/core/either'
 import { QuestionCommentsRepository } from '@/domain/forum/application/repositories/question-comments-repository'
-import { ResourceNotFoundError } from './errors/resource-not-found-error'
-import { NotAllowedError } from './errors/not-allowed-error'
+import { ResourceNotFoundError } from '@/core/errors/errors/resource-not-found-error'
+import { NotAllowedError } from '@/core/errors/errors/not-allowed-error'
 
 interface DeleteQuestionOnCommentUseCaseRequest {
   authorId: string
@@ -14,7 +14,7 @@ type DeleteQuestionOnCommentUseCaseResponse = Either<
 >
 
 export class DeleteQuestionOnCommentUseCase {
-  constructor(private questionCommentsRepository: QuestionCommentsRepository) {}
+  constructor(private questionCommentsRepository: QuestionCommentsRepository) { }
 
   async execute({
     authorId,
